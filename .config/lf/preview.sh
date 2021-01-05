4174a2652b609404b366d407de91b3acc5070865
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2021-01-06 00:39:09 +0100

    Update preview.sh

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -2,10 +2,26 @@
 # Preview script built for use with lf
 LINES=30
 case "$1" in
-    *.png|*.jpg|*.jpeg|*.mkv|*.mp4) mediainfo "$1";;
-    *.zip) zipinfo "$1";;
-    *.tar.gz) tar -ztvf "$1";;
-    *.tar.bz2) tar -jtvf "$1";;
-    *.tar) tar -tvf "$1";;
-    *) highlight "$1" -O ansi --force;;
+	*.tgz|*.tar.gz) tar tzf "$1";;
+	*.tar.bz2|*.tbz2) tar tjf "$1";;
+	*.tar.txz|*.txz) xz --list "$1";;
+	*.tar) tar tf "$1";;
+	*.zip|*.jar|*.war|*.ear|*.oxt) unzip -l "$1";;
+	*.rar) unrar l "$1";;
+	*.7z) 7z l "$1";;
+	*.[1-8]) man "$1" | col -b ;;
+	*.o) nm "$1" | less ;;
+	*.iso) isoinfo --no-header -l "$1";;
+	*.torrent) transmission-show "$1";;
+	*.iso) iso-info --no-header -l "$1";;
+	*.odt,*.ods,*.odp,*.sxw) odt2txt "$1";;
+	*.doc) catdoc "$1" ;;
+	*.docx) docx2txt "$1" - ;;
+	*.csv) cat "$1" | sed s/,/\\n/g ;;
+	*.bmp|*.jpg|*.jpeg|*.png|*.xpm) mediainfo "$1";;
+	*.avi|*.mp4|*.wmv|*.dat|*.3gp|*.ogv|*.mkv|*.mpg|*.mpeg|*.vob|*.fl[icv]|*.m2v|*.mov|*.webm|*.ts|*.mts|*.m4v|*.r[am]|*.qt|*.divx) mediainfo "$1";;
+	*.pdf) pdftotext "$1" - | less;;
+	*.epub) mediainfo "$1";;
+    *.wav|*.mp3|*.flac|*.m4a|*.wma|*.ape|*.ac3|*.og[agx]|*.spx|*.opus|*.as[fx]|*.flac) exiftool "$1";;
+	*) highlight --out-format ansi "$1" || batcat "$1";;
 esac
