0b0a2fdc0b10c0c82d7d3127c469b463b9d441bb
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2021-08-17 01:16:46 +0200

    Update lf-preview-only-on-focus.sh

--- .config/lf/lf-preview-only-on-focus.sh
+++ .config/lf/lf-preview-only-on-focus.sh
@@ -3,9 +3,9 @@
 bspc subscribe node_focus | while read line
 do
 	isLfFocused=$(xprop -id $(echo "$line" | awk '{print $4}') | grep 'WM_NAME(STRING) = "lf"' | wc -l)
-	if [ "$isLfFocused" == "0" ]; then
-	    ~/Applications/lf -remote 'send set nopreview' && ~/Applications/lf -remote 'send set ratios 1:2'
+	if [ "$isLfFocused" = "0" ]; then
+	    ~/bin/lf -remote 'send set nopreview' && ~/bin/lf -remote 'send set ratios 1:2'
 	else
-	    ~/Applications/lf -remote 'send set preview' && ~/Applications/lf -remote 'send set ratios 1:2:3'
+	    ~/bin/lf -remote 'send set preview' && ~/bin/lf -remote 'send set ratios 1:2:3'
 	fi
 done
