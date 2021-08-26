663e292245b0798d9a6a70a139b21e089d3309b4
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2021-08-26 10:12:26 +0200

    Update lf-preview-only-on-focus.sh

--- .config/lf/lf-preview-only-on-focus.sh
+++ .config/lf/lf-preview-only-on-focus.sh
@@ -2,7 +2,7 @@
 
 bspc subscribe node_focus | while read line
 do
-	isLfFocused=$(xprop -id $(echo "$line" | awk '{print $4}') | grep 'WM_NAME(STRING) = "lf"' | wc -l)
+	isLfFocused=$(xprop -id $(echo "$line" | awk '{print $4}') | grep '^WM_CLASS(STRING) = "lf"' | wc -l)
 	if [ "$isLfFocused" = "0" ]; then
 	    ~/bin/lf -remote 'send set nopreview' && ~/bin/lf -remote 'send set ratios 1:2'
 	else
